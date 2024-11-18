37ea5795861a43d54a7070678f8a1b62ac6165ec
Felipe Pacheco Zamorano <132003694+FelipeJackFox@users.noreply.github.com>
2024-11-17 21:05:38 -0600

    Delete Player.cpp

--- Player.cpp
+++
@@ -1,7 +0,0 @@
-#include "Player.h"
-#include <string> 
-
-// Constructor implementation
-Player::Player(std::string name) : Entity(100, 10, 5.0f, 1.5f, 0), name(name), experienceLevel(1) {}
-
-// Method implementations
