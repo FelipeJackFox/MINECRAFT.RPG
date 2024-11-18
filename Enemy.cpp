f45f51b4cb0ed23a6a8a9b1008ea32241dd011c1
Felipe Pacheco Zamorano <132003694+FelipeJackFox@users.noreply.github.com>
2024-11-17 21:07:04 -0600

    Delete Enemy.cpp

--- Enemy.cpp
+++
@@ -1,9 +0,0 @@
-#include "Enemy.h"
-
-// Constructor implementation
-Enemy::Enemy(int health, int damage, float critical_damage_prob, 
-             float critical_damage_multiplier, int resistance) : 
-             Entity(health, damage, critical_damage_prob, 
-             critical_damage_multiplier, resistance) {}
-
-// Method implementations
