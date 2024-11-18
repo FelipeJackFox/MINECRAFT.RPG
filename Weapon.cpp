fb9de4a69dfd3944bf2d38e9a82f8f50c03612a2
Felipe Pacheco Zamorano <132003694+FelipeJackFox@users.noreply.github.com>
2024-11-17 21:04:23 -0600

    Delete Weapon.cpp

--- Weapon.cpp
+++
@@ -1,5 +0,0 @@
-#include "Weapon.h"
-
-// Constructor implementation
-Weapon::Weapon(int damage, float critical_chance) : 
-  Item(ItemType::WEAPON), damage(damage), critical_chance(critical_chance) {}
