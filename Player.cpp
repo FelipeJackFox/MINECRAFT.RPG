7174331edf0f04d0505c8c1df9063992f0c9f6a0
Felipe Pacheco Zamorano <132003694+FelipeJackFox@users.noreply.github.com>
2024-11-23 11:52:29 -0600

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -1,7 +1,7 @@
 #include "Player.h"
 
 Player::Player(const std::string& name)
-    : Entity(), experienceLevel(0), experiencePoints(0), name(name) {}
+    : Entity(), experienceLevel(0), experiencePoints(0), experienceThreshold(40), pointsToAllocate(0), name(name) {}
 
 int Player::getExperienceLevel() const {
     return experienceLevel;
@@ -19,6 +19,14 @@ void Player::setExperiencePoints(int points) {
     experiencePoints = points;
 }
 
+int Player::getExperienceThreshold() const {
+    return experienceThreshold;
+}
+
+int Player::getPointsToAllocate() const {
+    return pointsToAllocate;
+}
+
 std::string Player::getName() const {
     return name;
 }
@@ -27,17 +35,59 @@ void Player::setName(const std::string& name) {
     this->name = name;
 }
 
+void Player::setResistance(int resistance) {
+    this->resistance = resistance;
+}
+
+void Player::setCriticalDamagePercent(float percent) {
+    criticalDamagePercent = percent;
+}
+
+void Player::setCriticalDamageMultiplier(float multiplier) {
+    criticalDamageMultiplier = multiplier;
+}
+
 void Player::levelUp() {
     experienceLevel++;
-    // Logic to increase a base stat upon leveling up
-    damage += 5; // Example: Increase damage by 5
-    health += 10; // Example: Increase health by 10
+    experiencePoints = 0; // Reset experience points upon level-up
+    pointsToAllocate += 3;
+    experienceThreshold += (experienceLevel * 10); // Increase thresholdsssss
 }
 
 void Player::gainExperience(int points) {
     experiencePoints += points;
-    while (experiencePoints >= 10) {
-        experiencePoints -= 10;
+    while (experiencePoints >= experienceThreshold) {
+        experiencePoints -= experienceThreshold;
         levelUp();
     }
+}
+
+void Player::allocatePointsToStat(int choice) {
+    if (pointsToAllocate <= 0) {
+        return; // No points available to allocate
+    }
+
+    switch (choice) {
+        case 1: // Increase Health
+            setMaxHealth(getMaxHealth() + 20); // Increase max health
+            setHealth(getHealth() + 20);       // Increase current health proportionally
+            break;
+        case 2: // Increase Damage
+            setDamage(getDamage() + 4);
+            break;
+        case 3: // Increase Resistance
+            setResistance(getResistance() + 3);
+            break;
+        case 4: // Increase Critical Hit Chance
+            setCriticalDamagePercent(getCriticalDamagePercent() + 0.02f);
+            break;
+        case 5: // Increase Critical Damage Multiplier
+            setCriticalDamageMultiplier(getCriticalDamageMultiplier() + 0.15f);
+            break;
+        default:
+            // Invalid choice
+            return;
+    }
+
+    pointsToAllocate--; // Decrease the points available for allocation and repeat until no points to allocate
 }
