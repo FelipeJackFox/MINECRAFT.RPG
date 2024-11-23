7938410f45a465afd0176fdf1a40c16724ecd9cd
Felipe Pacheco Zamorano <132003694+FelipeJackFox@users.noreply.github.com>
2024-11-23 11:51:05 -0600

    Update Player.h

--- Player.h
+++ Player.h
@@ -7,7 +7,9 @@
 class Player : public Entity {
 private:
     int experienceLevel;
-    int experiencePoints;
+    int experiencePoints; // Tracks current experience progress
+    int experienceThreshold; // Dynamic threshold for leveling up
+    int pointsToAllocate; // Tracks points available for stat allocation
     std::string name;
 
 public:
@@ -20,12 +22,20 @@ public:
     int getExperiencePoints() const;
     void setExperiencePoints(int points);
 
+    int getExperienceThreshold() const;
+    int getPointsToAllocate() const;
+
     std::string getName() const;
     void setName(const std::string& name);
 
+    void setResistance(int resistance);
+    void setCriticalDamagePercent(float percent);
+    void setCriticalDamageMultiplier(float multiplier);
+
     // Methods
     void levelUp();
-    void gainExperience(int experiencePoints);
+    void gainExperience(int points);
+    void allocatePointsToStat(int choice);
 };
 
 #endif // PLAYER_H
