82b093376066ed1044e2298ff814729fd60978ce
Felipe Pacheco Zamorano <132003694+FelipeJackFox@users.noreply.github.com>
2024-11-23 11:54:52 -0600

    Update Enemy.h

--- Enemy.h
+++ Enemy.h
@@ -2,14 +2,25 @@
 #define ENEMY_H
 
 #include "Entity.h"
-#include "Player.h"
+#include <string>
 
 class Enemy : public Entity {
+private:
+    std::string name;    // Enemy name
+    int experiencePoints; // Experience points granted when defeated
+
 public:
     Enemy();
 
-    // Methods
-    void attackPlayer(Player& player);
+    // Getters and Setters
+    void setName(const std::string& name);
+    std::string getName() const;
+
+    void setExperiencePoints(int exp);
+    int getExperiencePoints() const;
+
+    // New method to get plain ASCII art (no color), before I was defining the ASCII directly inside of the main file, now I jsut apply color there.
+    std::string getAsciiArt() const;
 };
 
 #endif // ENEMY_H
